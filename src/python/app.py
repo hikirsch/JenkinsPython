66794dc760633d4860fd3d4425bd2f0bbe9fa0b5
Adam S. Kirschner <me@adamskirschner.com>
2013-09-08 23:05:13 -0400

    working on closure compiler

--- src/python/app.py
+++ src/python/app.py
@@ -14,21 +14,57 @@ from helpers.ArgumentsHelper import ArgumentsHelper
 from commands.Copy import Copy
 
 
-class Jenkins:
+class Settings:
 	BUILD_ASSETS_PATH = "build-assets"
 	TARGET_PATH = "target"
 	SOURCE_PATH = "src"
 
+
+class Compile():
+	def __init(self, arg_helper):
+		self.arg_helper = arg_helper
+
+		command = self.arg_helper.getNextArgument()
+
+		try:
+			methodToCall = getattr(self, command)
+
+			if methodToCall is not None:
+				methodToCall()
+
+		except AttributeError:
+			raise Exception("Command Not Found")
+
+	def fake(self):
+		projectType = self.argHelper.getNextArgument()
+		src = Settings.SOURCE_PATH + os.path.sep + projectType
+
+		if os.path.exists(src):
+			Copy(src, Settings.TARGET_PATH)
+		else:
+			raise Exception("source path '%s' does not exist" % src)
+
+	def closure(self):
+		from compilers.Closure import ClosureCompiler
+
+		ClosureCompiler()
+
+		pass
+
+
+class Jenkins:
 	version = "0.0.1"
 
-	argHelper = None
+	arg_helper = None
+
+	compile = Compile()
 
 	def __init__(self):
-		self.argHelper = ArgumentsHelper(argv)
+		self.arg_helper = ArgumentsHelper(argv)
 
 	def run(self):
 		try:
-			command = self.argHelper.getCommand()
+			command = self.arg_helper.getCommand()
 
 			lst = [word[0].upper() + word[1:] for word in command.split("-")]
 			command = "".join(lst)
@@ -41,7 +77,7 @@ class Jenkins:
 
 				if methodToCall is not None:
 					methodToCall()
-			except AttributeError as e:
+			except AttributeError:
 				raise Exception("Command Not Found")
 
 			print "DONE"
@@ -51,7 +87,7 @@ class Jenkins:
 			print EQUALS_LINE
 			print "Exception"
 			print EQUALS_LINE
-			print "An error occured! "
+			print "An error occurred! "
 			print e
 			print DASH_LINE
 			print traceback.format_exc()
@@ -60,44 +96,37 @@ class Jenkins:
 
 
 	def clean(self):
-		path = Path(self.TARGET_PATH)
+		path = Path(Settings.TARGET_PATH)
 
 		if path.exists():
 			shutil.rmtree(path.path)
-			print "Removed '%s'" % self.TARGET_PATH
+			print "Removed '%s'" % Settings.TARGET_PATH
 		else:
 			print "Nothing to clean"
 
+	def compile(self):
+		Compile(self.arg_helper)
+
 	def copy(self):
-		src = self.argHelper.getNextArgument()
-		dest = self.argHelper.getNextArgument()
+		src = self.arg_helper.getNextArgument()
+		dest = self.arg_helper.getNextArgument()
 
 		Copy(src, dest)
 
 	def merge(self):
-		src = self.argHelper.getNextArgument()
-		dest = self.argHelper.getNextArgument()
+		src = self.arg_helper.getNextArgument()
+		dest = self.arg_helper.getNextArgument()
 
 		Merge(src, dest)
 
-	def fakeCompile(self):
-		projectType = self.argHelper.getNextArgument()
-		src = self.SOURCE_PATH + os.path.sep + projectType
-
-		if os.path.exists(src):
-			Copy(src, self.TARGET_PATH)
-		else:
-			raise Exception("source path '%s' does not exist" % src)
-
-
 	def deploy(self):
-		dest = self.argHelper.getNextArgument()
+		dest = self.arg_helper.getNextArgument()
 
-		Copy(self.TARGET_PATH, dest)
+		Copy(Settings.TARGET_PATH, dest)
 
 	def buildAssets(self):
-		when = self.argHelper.getNextArgument() + "-build"
-		env = self.argHelper.getNextArgument()
+		when = self.arg_helper.getNextArgument() + "-build"
+		env = self.arg_helper.getNextArgument()
 
 		if env is None and "ENV" in os.environ:
 			env = os.environ["ENV"]
@@ -105,20 +134,14 @@ class Jenkins:
 		if env is None:
 			raise Exception("No environment found")
 		else:
-			when = self.caseInsensativeFolder(self.BUILD_ASSETS_PATH, when)
+			when = self.caseInsensativeFolder(Settings.BUILD_ASSETS_PATH, when)
 			path = self.caseInsensativeFolder(when, env)
 
 			if os.path.exists(path):
-				Merge(path, self.TARGET_PATH)
+				if 
+				Merge(path, Settings.TARGET_PATH)
 			else:
 				raise Exception("Path '%s' was not found!" % path)
-
-	def closure(self):
-		from compilers.Closure import ClosureCompiler
-
-		ClosureCompiler()
-
-		pass
 
 	def caseInsensativeFolder(self, path, when):
 		files = os.listdir(path)
--- src/python/compilers/Closure.py
+++ src/python/compilers/Closure.py
@@ -1,19 +1,39 @@
 import os
-from app import Jenkins
+from app import Settings
+from model.Path import Path
 
 
 class ClosureCompiler:
 	JAR_FILE_NAME = "closure-compiler.jar"
 
-	CONFIG_PATH = Jenkins.BUILD_ASSETS_PATH + os.path.sep + "minify" + os.path.sep + "js.txt"
+	CONFIG_PATH = Path(Settings.BUILD_ASSETS_PATH + os.path.sep + "minify" + os.path.sep + "js.txt")
 
 	compiler_path = None
 
 	def __init__(self):
-		self.compiler_path = os.path.dirname(os.path.abspath(__file__)) + os.path.sep + self.JAR_FILE_NAME
+		self.compiler_path = Path(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + self.JAR_FILE_NAME)
 
-		print "Compiler Path: %s" % self.compiler_path
-		print "Config Path: %s" % self.CONFIG_PATH
+		print "Compiler Path: %s" % self.compiler_path.path
+		print "Config Path: %s" % self.CONFIG_PATH.path
+
+		if self.CONFIG_PATH.exists():
+			self.run()
 
 	def run(self):
-		pass
+		open_file = open("sample.txt")
+
+		while 1:
+			line = open_file.readline()
+
+			if not line:
+				break
+
+			firstCharacter = line[:1]
+
+			if firstCharacter == "+":
+				pass # should just append
+			else:
+				pass # run compiler
+
+
+
