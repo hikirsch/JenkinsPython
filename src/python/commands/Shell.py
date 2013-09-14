e83e85b68061f91c27adf2c353a12ddb97eec321
Adam S. Kirschner <me@adamskirschner.com>
2013-09-14 17:55:20 -0400

    ignoring host check

--- src/python/commands/Shell.py
+++ src/python/commands/Shell.py
@@ -20,6 +20,6 @@ class Shell(BaseCommand):
 			if not self.path.is_absolute:
 				path = "./" + path
 
-			command = [executable, self.path.get_ssh_path(), path ]
+			command = [executable, "-o", "UserKnownHostsFile=/dev/null","-o", "StrictHostKeyChecking=no", self.path.get_ssh_path(), path]
 
 			Execute().run(command)
