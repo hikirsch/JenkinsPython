9ad08c828d4bc8c4ad791edde77354bf4eb5d3b6
Adam S. Kirschner <me@adamskirschner.com>
2014-09-24 08:32:18 -0400

    git revision

--- src/python/commands/GitRevision.py
+++ src/python/commands/GitRevision.py
@@ -1,19 +1,23 @@
 import os
+
 from helpers.Execute import Execute
 
 
 class GitRevision:
-
     def __init__(self):
         pass
 
-    def write_revision(self, filename=".git_revision"):
-
+    def write_revision(self, folder="target", filename=".git_revision"):
         output = Execute().get_output(['git', 'describe', '--tags', '--always', 'HEAD'])
 
         if os.path.exists(filename):
             os.remove(filename)
 
-        file = open(filename, "w")
-        file.write("".join(output).strip())
+        full_path = "%s/%s" % ( folder, filename )
+
+        file = open(full_path, "w")
+        revision = "".join(output).strip()
+        file.write(revision)
         file.close()
+
+        print "Git revision %s written to %s" % ( revision, full_path )
